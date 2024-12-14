a19bc0337db3c8a89fd1960aa7238dae5cb5da31
ShifarEmtiuz07 <shifar16@cse.pstu.ac.bd>
2024-12-14 12:56:40 +0600

    modified

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! @v1 branch';
+    return 'Hello World! @v1 but its a main branch'
   }
 }
